5a9334cc47fefafaf307fd5cd311ed050b9d592d
Jerry Cheng <sj890190@gmail.com>
2022-01-12 11:15:21 +0800

    撰寫與測試, 資料與中心點的距離

--- src/initCluster/K_means.java
+++ src/initCluster/K_means.java
@@ -5,6 +5,7 @@
 package initCluster;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -18,9 +19,11 @@ public class K_means {
     public static void main(String[] args) {
         // TODO code application logic here
         double[] list = {1,2,3,4,50,51,52,53,500,501,502,503};
-        int num_of_clusters = 3;
+        int num_of_clusters = 3;   
         
-        getCenter(list, num_of_clusters);
+        getDistance(List.of(1.0, 502.0, 503.0), list);
+        
+        
         
         
         //List<List<Double>> res = algorithm(list, num_of_clusters);
@@ -29,11 +32,38 @@ public class K_means {
     
     public static List<List<Double>> algorithm(double[] list, int k){
         List<List<Double>> res = new ArrayList<>(); // 存放結果
-        List<Double> center; // 存放中心點
+        List<Double> center = getCenter(list, k); // 存放中心點
         
         return res;
     }
     
+    //已經驗證過是預期的結果
+    public static List<List<Double>> getDistance(List<Double> center, double[] data){
+        List<List<Double>> res = new ArrayList<>(); // 存放分組結果        
+        for(int i=0; i<data.length; i++){//以資料跟中心點做比對
+            List<Double> distance = new ArrayList<>(); // 存放與中心點的距離
+            for(int j=0; j<center.size(); j++){
+                //取得資料與給個中心點的距離
+                distance.add(Math.abs(data[i] - center.get(j)));
+            }
+            res.add(distance);
+        }
+        res.stream().map(list -> list.toString()).forEach(s -> System.err.println(s));
+        return res;     
+        
+//        for(int i=0; i<center.size(); i++){//以中心點跟資料做比對
+//            List<Double> distance = new ArrayList<>(); // 存放與中心點的距離
+//            for(int j=0; j<data.length; j++){
+//                //取得資料與給個中心點的距離
+//                distance.add(Math.abs(center.get(i) - data[j]));
+//            }
+//            res.add(distance);
+//        }
+//        res.stream().map(list -> list.toString()).forEach(s -> System.err.println(s));
+//        return res;
+    }
+    
+    //已經驗證過是預期的結果
     public static List<Double> getCenter(double[] data, int k){
         List<Double> res = new ArrayList<>();
         //需要取到k個中心點
@@ -46,9 +76,8 @@ public class K_means {
             }
         }
         //由小到大排序
-        res = res.stream().sorted().collect(Collectors.toList());
-        System.out.println(res.toString());
-        return res;
+        System.out.println(res.stream().sorted().collect(Collectors.toList()).toString());
+        return res.stream().sorted().collect(Collectors.toList());
     }
     
 }
